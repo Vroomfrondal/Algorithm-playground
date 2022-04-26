ae0cce7bfb80dc4325d6f1778d0249fd36a22f5e
Vroomfrondal <lionscantcode@gmail.com>
2022-04-26 15:11:15 -0500

    404-sumOfLeftLeaves

---
+++ Leetcode/Easy/404-sumOfLeftLeaves.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree r.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+//var sumOfLeftLeaves = function (root) {
+// set a sum counter
+
+// function traverseTree: root as argument {
+// check if root is absent and return from the function if so (avoid infinite loop)
+
+// if root.left ...
+// 2 paths: root.left and root.right
+// check if root.left.left and root.left.right are absent (meaning no child nodes), and add root.left.val to sum
+// recursive traverseTree function with root.left and root.right
+//}
+
+//}
