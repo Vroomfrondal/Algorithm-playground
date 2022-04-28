8d60b31a63f26730a3f6c0c110ca0e7f9acea82d
Vroomfrondal <lionscantcode@gmail.com>
2022-04-28 13:29:49 -0500

    Solved 404 - 100/100 test cases

--- Leetcode/Easy/404-sumOfLeftLeaves.js
+++ Leetcode/Easy/404-sumOfLeftLeaves.js
@@ -1,20 +1,25 @@
-// passes leetcode test cases but fails soemthing like [5, null, 7, 6, 10] and I don't know why
+// Recursive solution that beats 99% in time
 var sumOfLeftLeaves = function (root) {
     let sum = 0
 
-    function traverseTree(root) {
+    traverseTree = (root) => {
+        // base case to end recursion
         if (!root) {
             return
         }
 
+        // Child nodes? Check them!
         if (root.left) {
-            //checking children nodes
-            if (root.left.left === null) {
+            // Add child node to sum because its a leaf (children absent)
+            if (root.left.left === null && root.left.right === null) {
                 sum += root.left.val
-                traverseTree(root.right)
             }
         }
+        // traverse through binary tree subtree's
+        traverseTree(root.left)
+        traverseTree(root.right)
     }
+    // invoke on main root
     traverseTree(root)
     return sum
 }
