c2530c928faa2789c83b0c3f12fa8da3a28c4876
Vroomfrondal <lionscantcode@gmail.com>
2022-05-06 08:59:31 -0500

    Reformat 404

--- Leetcode/Easy/404-sumOfLeftLeaves.js
+++ Leetcode/Easy/404-sumOfLeftLeaves.js
@@ -1,25 +1,20 @@
-// Only beats 44.6%. instead of checking for !, check if child nodes are === null!!
 var sumOfLeftLeaves = function (root) {
     let sum = 0
 
-    traverseTree = (currRoot) => {
-        // base case to end recursion
-        if (!currRoot) {
+    traverseTree = (root) => {
+        if (!root) {
             return
-        }
-
-        // Else: if current root has a child, check that sub tree
-        if (currRoot.left) {
-            // Add child node to sum because its a leaf (children absent)
-            if (!currRoot.left.left && !currRoot.left.right) {
-                sum += currRoot.left.val
+        } else {
+            if (root.left) {
+                if (!root.left.left && !root.left.right) {
+                    sum += root.left.val
+                }
             }
+            traverseTree(root.left)
+            traverseTree(root.right)
         }
-        // traverse through binary tree subtree's
-        traverseTree(currRoot.left)
-        traverseTree(currRoot.right)
     }
-    // invoke on main root
+
     traverseTree(root)
     return sum
 }
