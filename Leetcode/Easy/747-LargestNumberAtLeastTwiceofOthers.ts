bff805c0ef6e25c85fcce8b56f6601a1826d3edf
Vroomfrondal <lionscantcode@gmail.com>
2022-07-15 20:52:57 -0500

    Added comments to 747 (Easy)

--- Leetcode/Easy/747-LargestNumberAtLeastTwiceofOthers.ts
+++ Leetcode/Easy/747-LargestNumberAtLeastTwiceofOthers.ts
@@ -1,24 +1,27 @@
-// Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+// ! Prompt: Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+// ! Solution: Faster than 60% of submissions,
 function dominantIndex(nums: number[]) {
     let answer: number = 0
-    // apply lets uus pass array of data as parameters to a function
-    const largestValue = Math.max.apply(null, [...nums])
-    const largestIndex = nums.indexOf(largestValue)
+    // Unpack all items in nums array and take max value as well as index of max value
+    const largestValue: number = Math.max(...nums)
+    const largestIndex: number = nums.indexOf(largestValue)
 
-    // Filter largest value from array
-    const filtered = nums.filter((num) => num !== largestValue)
+    // Filter out the largestValue from array since we don't need to compare it to itself
+    const filteredArr = nums.filter((num) => num !== largestValue)
 
-    // Loop through array and check if largestValue is > n * 2
-    for (let i = 0; i < filtered.length; i++) {
-        const currNum = filtered[i]
+    // Loop through new array and check if largestValue is ever >= n * 2
+    for (let i = 0; i < filteredArr.length; i++) {
+        const currNum = filteredArr[i]
 
-        // We Exit this loop if current n * 2 ever is larger than the largest num
+        // If the current loop itteration is *ever* more than the largestValue, return -1.
         if (largestValue >= currNum * 2) answer = largestIndex
         else return (answer = -1)
     }
+
     return answer
 }
 
+// Test Cases
 console.log(dominantIndex([3, 6, 1, 0]))
 console.log('-------------------')
 console.log(dominantIndex([0, 0, 0, 2]))
