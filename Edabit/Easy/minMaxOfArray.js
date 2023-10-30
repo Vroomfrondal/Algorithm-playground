6be0a0e31afa4ff3675a004f5455171ed155d79a
Vroomfrondal <lionscantcode@gmail.com>
2023-10-30 10:37:18 -0500

    Remove Edabit

--- Edabit/Easy/minMaxOfArray.js
+++
@@ -1,8 +0,0 @@
-function minMax(arr) {
-    const min = Math.min(...arr) // ES6 destructuring assigment
-    const max = Math.max(...arr) // ES6 destructuring assigment
-    console.log(min, max)
-}
-
-minMax([1, 2, 3, 4])
-minMax([24, 50, 33, 25])
